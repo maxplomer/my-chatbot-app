9d9669d626d8a6cde4d1c860a4f545cddf078d6e
Max Plomer <maxplomer@gmail.com>
2019-01-20 01:28:16 -0500

    Updates

---
+++ precache-manifest.bd11d49a9948c8992ef328e31bf40a09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a133a815535b400a85bb",
+    "url": "/my-chatbot-app/static/css/main.afdf74c7.chunk.css"
+  },
+  {
+    "revision": "a133a815535b400a85bb",
+    "url": "/my-chatbot-app/static/js/main.a133a815.chunk.js"
+  },
+  {
+    "revision": "76b6102a6fb88f31cd4f",
+    "url": "/my-chatbot-app/static/js/1.76b6102a.chunk.js"
+  },
+  {
+    "revision": "b9b7aa1c7a1a3ff0dd6d",
+    "url": "/my-chatbot-app/static/js/runtime~main.b9b7aa1c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-chatbot-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ca3579830e8a5331ef7104523fc2eb32",
+    "url": "/my-chatbot-app/index.html"
+  }
+];
