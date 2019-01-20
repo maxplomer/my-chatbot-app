98febc174717cc867f5ae647bc4b463e99928b4e
Max Plomer <maxplomer@gmail.com>
2019-01-20 01:16:28 -0500

    initial chat react app and Chatbot.js class (#1)
    * add date and simple array of messages
    
    * submitChatForm
    
    * add new message
    
    * add author attribute in message data
    
    * style bot messages to left
    
    * adjust styling of chat
    
    * callChatbot
    
    * add componentDidMount
    
    * import Chatbot class
    
    * export chatbot
    
    * create chatbot instance
    
    * add ;
    
    * clean up default content
    
    * add homepage to package.json

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Chatbot from './Chatbot';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: new Date(),
+      messages: [
+        {author: "bot", text: "helloworld1234 🤖"},
+        {author: "user", text: "helloworld5678"},
+        {author: "bot", text: "helloworld9012 🤖"}
+      ]
+    };
+    this.textInput = React.createRef();
+    this.submitChatForm = this.submitChatForm.bind(this);
+  }
+
+  componentDidMount() {
+    this.Chatbot = new Chatbot();
+  }
+
+  submitChatForm(event) {
+    event.preventDefault();
+
+    //Add user message to state
+    let messages = this.state.messages;
+    let userMessage = this.textInput.current.value;
+    this.textInput.current.value = "";
+    messages.push({author: "user", text: userMessage});
+    this.setState({
+      messages: messages
+    });
+
+    //Call chatbot
+    let chatbotReponse = this.Chatbot.talk(userMessage);
+
+    switch(chatbotReponse.action) {
+      case 'showContactForm':
+
+        break;
+      case 'showCreateProjectForm':
+
+        break;
+      case 'showPortfolioSlideshow':
+
+        break;
+      default:
+        messages.push({author: "bot", text: chatbotReponse.botMessage});
+        this.setState({
+          messages: messages
+        });
+    }
+  }
+
   render() {
+    var textInputStyle = {width: '50%', padding: '5px', margin: '20px 0'};
+    var sendButtonStyle = {padding: '5px 10px', marginLeft: '10px', cursor: 'pointer'};
+    var messageStyle = {padding: '5px 10px'};
+
     return (
       <div className="App">
+        {this.state.date.toString()}
+        {this.state.messages.map(function(message, index) {
+          return (<div key={index} className={message.author} style={messageStyle}><span>{message.text}</span></div>);
+        })}
+        <form onSubmit={this.submitChatForm}>
+          <input type="text" ref={this.textInput} required style={textInputStyle}/>
+          <button type="submit" style={sendButtonStyle}>Send</button>
+        </form>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Max Plomer Chatbot App
           </p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://maxplomer.github.io"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Portfolio
           </a>
         </header>
       </div>
---
+++ src/Chatbot.js
@@ -0,0 +1,7 @@
+class Chatbot {
+  talk() {
+    return {botMessage: "this is the bot message 5678 🤖", action: null};
+  }
+}
+
+export default Chatbot;
